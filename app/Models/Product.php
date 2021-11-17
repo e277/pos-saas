643d43437e485bed6f52d19e51eba518db895330
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-11-17 04:00:59 -0500

    Product Migration Updated, Sales Table Updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Product
@@ -22,18 +27,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property int $qty
  * @property string $status
  * @property string $desc
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Store[] $brand
+ * @property Carbon|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|Store[] $brand
  * @property-read int|null $brand_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Category[] $category
+ * @property-read Collection|Category[] $category
  * @property-read int|null $category_count
- * @property-read \App\Models\Store $store
- * @method static \Database\Factories\ProductFactory factory(...$parameters)
+ * @property-read Store $store
+ * @method static ProductFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
- * @method static \Illuminate\Database\Query\Builder|Product onlyTrashed()
+ * @method static Builder|Product onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Product query()
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereBrandId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereCategoryId($value)
@@ -51,13 +56,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereStoreId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Product whereWeight($value)
- * @method static \Illuminate\Database\Query\Builder|Product withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Product withoutTrashed()
- * @mixin \Eloquent
+ * @method static Builder|Product withTrashed()
+ * @method static Builder|Product withoutTrashed()
+ * @mixin Eloquent
  */
 class Product extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'brand_id',
@@ -72,13 +77,22 @@ class Product extends Model
         'desc'
     ];
 
-    public function brand(){
+    public function brand()
+    {
         return $this->belongsTo(Brand::class);
     }
-    public function category(){
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
-    public function store(){
+
+    public function store()
+    {
         return $this->belongsTo(Store::class);
+    }
+
+    public function invoice(){
+        return $this->hasMany(Invoice::class);
     }
 }
--- resources/views/livewire/sale-report/sales-report.blade.php
+++ resources/views/livewire/sale-report/sales-report.blade.php
@@ -5,7 +5,7 @@
                 <x-Proximity-Table.table-head title="Product Name"/>
                 <x-Proximity-Table.table-head title="Category"/>
                 <x-Proximity-Table.table-head title="Quantity"/>
-                <x-Proximity-Table.table-head title="Discounted"/>
+                <x-Proximity-Table.table-head title="Discount"/>
                 <x-Proximity-Table.table-head title="Price"/>
                 <x-Proximity-Table.table-head title="Expected Revenue"/>
                 <x-Proximity-Table.table-head title="Action"/>
@@ -14,13 +14,15 @@
             <x-slot name="tbody">
 
                 @forelse($products as $product)
+                    {{dd($products)}}
+{{--                    {{dd($product->invoice)}}--}}
                     <x-Proximity-Table.table-row>
                         <x-Proximity-Table.table-cell :title="$product->name"/>
                         <x-Proximity-Table.table-cell :title="$product->category->category_nm"/>
                         <x-Proximity-Table.table-cell :title="$product->qty"/>
-                        <x-Proximity-Table.table-cell :title="$product->sale_price"/>
-                        <x-Proximity-Table.table-cell :title="$product->price"/>
-                        <x-Proximity-Table.table-cell :title="$product->qty * $product->price - $product->sale_price"/>
+                        <x-Proximity-Table.table-cell :title="'$ '.number_format($product->sale_price,2)"/>
+                        <x-Proximity-Table.table-cell :title="'$ '.number_format($product->price,2)"/>
+                        <x-Proximity-Table.table-cell :title="'$ '.number_format($product->qty * $product->price - $product->sale_price,2)"/>
                         <x-Proximity-Table.table-cell>
                             <x-slot name="title">
                                 <button class="px-4 py-2 rounded bg-gray-500 hover:bg-green-500 duration-300 text-white">View</button>
