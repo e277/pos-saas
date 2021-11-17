1b639c260a935e2f094278d3faf2d9f391b62b49
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-11-17 03:58:09 -0500

    User Factory Done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,6 +11,7 @@ class UserFactory extends Factory
      * Define the model's default state.
      *
      * @return array
+     * @throws \Exception
      */
     public function definition()
     {
@@ -18,22 +19,44 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'user_type'=>rand(0,2),
+            'user_type' => random_int(0, 3),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
+
+    public function customer()
     {
         return $this->state(function (array $attributes) {
             return [
-                'email_verified_at' => null,
+                'user_type' => 2,
+            ];
+        });
+    }
+
+    public function merchant()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'user_type' => 1,
+            ];
+        });
+    }
+
+    public function superAdmin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'user_type' => 0,
+            ];
+        });
+    }
+
+    public function salesAccountant()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'user_type' => 3,
             ];
         });
     }
